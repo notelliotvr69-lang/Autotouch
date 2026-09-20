f75e30e13f7b4869f04c9e47123c071dad5240a8
notelliotvr69-lang <notelliotvr69@gmail.com>
2026-09-19 20:46:09 -0500

    Bump AutoTouch version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.autotouch.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
