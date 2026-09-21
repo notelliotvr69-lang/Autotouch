37e396c68c83694164a3c10ab38b7e3b65758dfb
notelliotvr69-lang <notelliotvr69@gmail.com>
2026-09-21 18:57:56 -0500

    Bump MotoFBT to 0.3

--- motofbt/app/build.gradle.kts
+++ motofbt/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.motofbt.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     buildTypes {
